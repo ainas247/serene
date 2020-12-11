e9056ec5a0cb523eeec1ab4f605b5db441bf7436
Emmanuel Aina <ainas247@gmail.com>
2020-12-12 00:20:25 +0100

    competed page

---
+++ main.js
@@ -0,0 +1,17 @@
+let toggl = document.querySelector(".toggle");
+let menuu = document.querySelector(".menu");
+
+toggl.addEventListener('click', myToggle);
+function myToggle() {
+    menuu.classList.toggle('lop');
+    console.log('ello');
+}
+
+// function myFunction() {
+//   var x = document.querySelector(".menu");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
