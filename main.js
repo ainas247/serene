940e972dc2827ebe6b078372b21c76c4b72d4729
Emmanuel Aina <ainas247@gmail.com>
2021-01-24 17:31:44 +0100

    Done

--- main.js
+++ main.js
@@ -4,14 +4,4 @@ let menuu = document.querySelector(".menu");
 toggl.addEventListener('click', myToggle);
 function myToggle() {
     menuu.classList.toggle('lop');
-    console.log('ello');
-}
-
-function myFunction() {
-  var x = document.querySelector(".menu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
 }
